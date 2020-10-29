7327137347d527b0457e19bc00d06f2c67f0d516
Santiago Yepes <santiagoyepescarrera@gmail.com>
2020-10-28 22:36:31 -0400

    added documentation and change functionality of /books/details

--- server/routes/books.js
+++ server/routes/books.js
@@ -34,14 +34,26 @@ router.get('/', (req, res, next) => {
 
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
+  
+  /* there are two ways of doing this.... 
+  1) pass an object with the field title and a second field as an empty object books:books - so the /books/details will fill their .values="" with empty content
+  2) pass an object with the field title and NO second field - so we need to add on /books/details on each form field an if statement using ejs, like this:
+  <% if (title == "Edit Book"){ %>           
+    value="<%= books.genre %>" 
+  <% } else {%>
+    required>  
+  <% } %>
 
-  res.render('books/details', { title: "Add Book" });
+  I CHOOSE 1) and did not change the details template
+  */
+  res.render('books/details', { title: "Add Book", books: book });
 
 });
 
 // POST process the Book Details page and create a new Book - CREATE
 router.post('/add', (req, res, next) => {
 
+  // create and assign new book fields
   let newBook = book({
     "title": req.body.title,
     "description": req.body.description,
@@ -50,6 +62,7 @@ router.post('/add', (req, res, next) => {
     "genre": req.body.genre
   });
 
+  // use mongo command to create new book
   book.create(newBook, (err, Book) =>{
     if(err) {
       console.log(err);
@@ -62,10 +75,13 @@ router.post('/add', (req, res, next) => {
 });
 
 // GET the Book Details page in order to edit an existing Book
+
+// crate new path by adding /edit to be organize with my urls
 router.get('/edit/:id', (req, res, next) => {
   
   let id = req.params.id;
 
+  // use mongo command to find book
   book.findById(id, (err, books) => {
     if(err) {
       console.log(err);
@@ -80,8 +96,10 @@ router.get('/edit/:id', (req, res, next) => {
 // POST - process the information passed from the details form and update the document
 router.post('/edit/:id', (req, res, next) => {
 
+  // retrieve book id from the req object
   let id = req.params.id
 
+  // Assign new book fields
   let updatedBook = book({
     "_id": id,
     "title": req.body.title,
@@ -91,6 +109,7 @@ router.post('/edit/:id', (req, res, next) => {
     "genre": req.body.genre
   });
 
+  // use mongo command to update book
   book.updateOne({_id: id}, updatedBook, (err) => {
       if(err) {
         console.log(err);
@@ -107,6 +126,7 @@ router.get('/delete/:id', (req, res, next) => {
 
   let id = req.params.id;
 
+  // use mongo command to delete book
   book.remove({_id: id}, (err) => {
     if(err) {
       console.log(err);
